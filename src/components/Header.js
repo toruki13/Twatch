6f7a21cc99f82253aa1f8820bbeda412a50e03ae
toruki13 <rogerpineda1314@gmail.com>
2021-06-09 01:10:44 -0600

    removed uncessary css

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,8 +16,9 @@ export default Header;
 
 const Container = styled.div`
   font-family: 'Roboto', sans-serif;
-
   padding: 1rem;
+  padding-bottom: 0.3rem;
+
   background-color: #94d2bd;
   display: flex;
   justify-content: space-between;
@@ -35,16 +36,8 @@ const Container = styled.div`
     border: none;
     cursor: pointer;
     text-align: center;
-    /*  padding-left: 2rem; */
     background: none;
     color: white;
     text-decoration: none;
-  }
-  a:nth-child(1) {
-    /*  padding-left: 0.5rem; */
-    /* flex: 1 1; */
-  }
-  a:nth-child(2) {
-    /*  padding-left: 3rem; */
   }
 `;
